fea0281fcb744cfacd5b604c0938b5685301efba
Chris Mahoney <cakestick@gmail.com>
2012-05-02 23:19:18 -0400

    title screen template

--- activeknowledge/js/init.js
+++ activeknowledge/js/init.js
@@ -1,5 +1,5 @@
 // Global game object
-var game;
+var maingame;
 
 // On load, run loadResources
 window.addEventListener('load', loadResources, false);
@@ -33,5 +33,38 @@ function loadResources()
 
 function main()
 {
-	alert('ahhh!');
+	// Create group for game to reside in
+	gbox.setGroups(['game']);
+	
+	// Create game object in 'game' group
+	maingame = gamecycle.createMaingame('game', 'game');
+	
+	maingame.gameTitleIntroAnimation=function(reset) {
+		if (reset) {
+			toys.resetToy(this, 'rising');
+		}
+		
+		gbox.blitFade(gbox.getBufferContext(), { alpha: 1 });
+		
+		toys.logos.linear(this, 'rising', {
+			image: 'logo',
+			sx:	   gbox.getScreenW()/2-gbox.getImage('logo').width/2,
+			sy:	   gbox.getScreenH(),
+			x:     gbox.getScreenW()/2-gbox.getImage('logo').width/2,
+			y:     20,
+			speed: 1
+		});
+	}
+	
+	maingame.pressStartIntroAnimation=function(reset) {
+		if (reset) {
+			toys.resetToy(this, "default-blinker");
+		} else {
+			toys.text.blink(this, "default-blinker", gbox.getBufferContext(), {font:"small",text:"PRESS Z TO START",valign:gbox.ALIGN_MIDDLE,halign:gbox.ALIGN_CENTER,dx:0,dy:Math.floor(gbox.getScreenH()/3),dw:gbox.getScreenW(),dh:Math.floor(gbox.getScreenH()/3)*2,blinkspeed:10});
+			return gbox.keyIsHit("a");
+		}
+	}
+	
+	// Start game loop
+	gbox.go();
 }
